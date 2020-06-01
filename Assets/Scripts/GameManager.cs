739216a63bce11cc7f4f4c6d1c87c06d515003f2
Vincent Manno <vincent.s.manno@gmail.com>
2020-05-31 20:43:22 -0400

    Basic Managers and Movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public PartyManager partyManager;
+    public DungeonManager dungeonManager;
+
+    private void Awake()
+    {
+        #region DontDestroyOnLoad
+        //Prexisting GameManager check
+        //if (GameObject.FindGameObjectWithTag("GameManager") != null)
+        //{
+        //    Destroy(this);
+        //}
+        //else
+        //{
+        //    DontDestroyOnLoad(this);
+        //}
+        #endregion
+
+        partyManager = FindOrCreateObjectWithTagAndType("PartyManager", typeof(PartyManager))
+            .GetComponent<PartyManager>();
+        dungeonManager = FindOrCreateObjectWithTagAndType("DungeonManager", typeof(DungeonManager))
+            .GetComponent<DungeonManager>();
+    }
+
+    private GameObject FindOrCreateObjectWithTagAndType(string tag, Type type)
+    {
+        GameObject GO = GameObject.FindGameObjectWithTag(tag);
+        if (GO == null)
+        {
+            Debug.Log($"Creating {tag}");
+            GO = new GameObject(tag, type);
+            GO.tag = tag;
+        }
+        return GO;
+    }
+}
---
+++ Assets/Scripts/PartyManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyManager : MonoBehaviour
+{
+    public GameObject party;
+
+    #region Party Movement Vars
+    public float partyMoveSpeed;
+    public bool partyCanMove = false;
+
+    public float waypointDetectionRange = 0.2f;
+    public int currentWaypointIndex;
+    public Transform[] waypoints;
+    #endregion
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (partyCanMove)
+        {
+            MovePartyToCurrentWaypoint();
+            CheckDistanceToCurrentWaypoint();
+        }
+    }
+
+    private void MovePartyToCurrentWaypoint()
+    {
+        party.transform.position = Vector2.MoveTowards(
+                party.transform.position,
+                waypoints[currentWaypointIndex].position,
+                partyMoveSpeed * Time.deltaTime);
+    }
+
+    private void CheckDistanceToCurrentWaypoint()
+    {
+        //If distance to next waypoint is less than a small detection value
+        if (Vector2.Distance(party.transform.position, waypoints[currentWaypointIndex].position) 
+            < waypointDetectionRange)
+        {
+            //Set next waypoint and check if at the last one
+            currentWaypointIndex++;
+            if (currentWaypointIndex == waypoints.Length)
+                partyCanMove = false;
+        }
+    }
+}
